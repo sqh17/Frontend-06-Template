8916e61a47ddbbf4d7cdd869d59ba1cc2bf17ef6
sqh17 <852104785@qq.com>
2020-12-15 20:17:00 +0800

    send完成

--- Week 08/toy-browser/client.js
+++ Week 08/toy-browser/client.js
@@ -19,12 +19,42 @@ class Request{
     }
     this.headers['Content-Length'] = this.bodyText.length;
   }
-  send(){
+  send(connection){
     return new Promise((resolve,reject)=>{
       // ……
-      let repsonse = new ResponseParser;
-      resolve("")
+      let parser = new ResponseParser;
+      
+      if(connection){
+        connection.write(this.toString())
+      }else{
+        connection = net.createConnection({
+          host:this.host,
+          port:this.port
+        },()=>{
+          connection.write(this.toString())
+        })
+      }
+      connection.on('data',data=>{
+        console.log(data.toString());
+        parser.receive(data.toString());
+        if(parser.isFinished){
+          resolve(parser.response);
+          connection.end()
+        }
+      })
+      connection.on('error',err=>{
+        reject(err);
+        connection.end()
+      })
+
     })
+  }
+  toString(){
+    return `${this.method} ${this.path} HTTP/1.1\r
+${Object.keys(this.headers).map(key=>`${key}:${this.headers[key]}`).join('\r\n')}\r
+\r
+${this.bodyText}
+    `
   }
 }
 
