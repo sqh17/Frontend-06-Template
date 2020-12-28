8afa441d717eb05ffc8112dceaaec71798bbc630
sqh17 <852104785@qq.com>
2020-12-28 20:24:33 +0800

    收集元素进行

--- Week 10/toy-browser/layout.js
+++ Week 10/toy-browser/layout.js
@@ -5,7 +5,7 @@ function layout(element){
   if(!element.computedStyle)
     return
   let elementStyle = getStyle(element);
-  // 目前只处理flex布局
+  // 目前只用flex布局
   if(elementStyle.display !== 'flex')
     return
   
@@ -103,6 +103,63 @@ function layout(element){
     crossBase = 0;
     crossSign = 1
   }
+
+  var isAutoMainSize = false; // 代表一行不会超过
+  if(!style[mainSize]){
+    elementStyle[mainSize] = 0;
+    for(var i = 0;i < items.length; i++){
+      var item = items[i];
+      elementStyle[mainSize] = elementStyle[mainSize] + item[mainSize] // 父元素的空间由子元素撑开
+    }
+    isAutoMainSize = true;
+  }
+
+
+  var flexLine = []; // 当前一行
+  var flexLines = [flexLine]; 
+
+  var mainSpace = elementStyle[mainSize]; // 主轴的空间
+  var crossSpace = 0; // 交叉轴的空间
+
+  for(var i = 0; i<items.length; i++){
+    var item = items[i];
+    var itemStyle = getStyle(item);
+
+    if(itemStyle[mainSize] === null){ // 没设主轴尺寸
+      itemStyle[mainSize] = 0;
+    }
+
+    if(itemStyle.flex){ // 若有flex属性，代表是可伸缩的，说明可放一行上
+      flexLine.push(item);
+    }else if(style.flexWrap === 'nowrap' && isAutoMainSize){ // 若是nowrap并且是不会超过
+      mainSpace -= itemStyle[mainSize];
+      if(itemStyle[crossSize] !== null && itemStyle[crossSize] !== (void 0)){
+        crossSpace = Math.max(crossSpace,itemStyle[crossSize]); // 记录循环每次的最大行高,交叉轴
+      }
+      flexLine.push(item)
+    } else { // 换行
+      if(itemStyle[mainSize] > style[mainSize]){ // 若子元素比父元素大
+        itemStyle[mainSize] = style[mainSize]
+      }
+      if(mainSpace < itemStyle[mainSize]){  // 主轴上的空间不足以容纳一个元素时，换行
+        flexLine.mainSpace = mainSpace; // ??? 实际剩余的尺寸和实际占的尺寸算出来??
+        flexLine.crossSpace = crossSpace;// ?????不懂，都覆盖了还有什么用
+        flexLine = [item];
+        flexLines.push(flexLine);
+        mainSpace = style[mainSize]; // 重置
+        crossSpace = 0;
+      }else{
+        flexLine.push(item)
+      }
+      if(itemStyle[crossSize] !== null && itemStyle[crossSize] !== (void 0)){
+        crossSpace = Math.max(crossSpace,itemStyle[crossSize]); // 记录循环每次的最大行高,交叉轴
+      }
+      mainSpace -= itemStyle[mainSize];
+    }
+  }
+  flexLine.mainSpace = mainSpace;
+
+  console.log(items)
 }
 
 
