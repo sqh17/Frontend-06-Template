046355b1e588c7d17e75948ebeef84118b50cf91
sqh17 <852104785@qq.com>
2021-01-29 22:38:30 +0800

    自动播放

--- Week 14/jsx/main.js
+++ Week 14/jsx/main.js
@@ -1,75 +1,54 @@
-function createElement(type, attributes, ...children) {
-  let element;
-  if(typeof type === 'string'){
-    element = new ElementWrapper(type)
-  }else{
-    element = new type
-  }
-  for (let i in attributes) {
-    element.setAttribute(i, attributes[i]);
-  }
-  for (let i of children) {
-    if(typeof i === 'string'){
-      i = new TextWrapper(i)
-    }
-    element.appendChild(i);
-  }
-  return element;
-}
-// 处理已知标签
-class ElementWrapper{
-  constructor(type){
-    this.root = document.createElement(type);
-  }
-  setAttribute(name,value){
-    this.root.setAttribute(name,value)
-  }
-  appendChild(child){
-    child.mountTo(this.root)
-  }
-  mountTo(parent){
-    parent.appendChild(this.root)
-  }
-}
-class TextWrapper{
-  constructor(content){
-    this.root = document.createTextNode(content);
-  }
-  setAttribute(name,value){
-    this.root.setAttribute(name,value)
-  }
-  appendChild(child){
-    child.mountTo(this.root)
-  }
-  mountTo(parent){
-    parent.appendChild(this.root)
-  }
-}
-// 处理未知标签
-class Div {
+import {createElement,Component} from './framework.js'
+class Carousel extends Component{
   constructor(){
-    this.root = document.createElement('div');
+    super()
+    this.attributes = Object.create(null);
+
   }
-  setAttribute(name,value){
-    this.root.setAttribute(name,value)
+  setAttribute(name, value) {
+    this.attributes[name] = value
   }
-  appendChild(child){
-    child.mountTo(this.root)
+  render(){
+    this.root = document.createElement("div");
+    this.root.classList.add("carousel");
+    for(let record of this.attributes.src){
+      let child = document.createElement('div');
+      child.style.backgroundImage = `url('${record}')`;
+      this.root.appendChild(child)
+    }
+    let currentIndex = 0
+    setInterval(() => {
+      let children = this.root.children;
+      let nextIndex = (currentIndex + 1) % children.length;
+
+      let current = children[currentIndex];
+      let next = children[nextIndex];
+
+      next.style.transition = "none";
+      next.style.transform = `translateX(${100 - nextIndex * 100 }%)`;
+
+      setTimeout(()=>{
+        next.style.transition = "";
+        current.style.transform = `translateX(${-100 - currentIndex * 100 }%)`
+        next.style.transform = `translateX(${- nextIndex * 100 }%)`
+        
+        currentIndex = nextIndex;
+      },16) // 16ms为一帧
+    },3000)
+    return this.root
   }
   mountTo(parent){
-    parent.appendChild(this.root)
+    parent.appendChild(this.render())
   }
 }
-let a = 
-  <Div id="a">
-    Hello World
-    <div>
-      <span>1</span>
-      <span>2</span>
-      <span>3</span>
-    </div>
-  </Div>
-;
-// document.body.appendChild(a);
 
+let d = [
+  'https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg',
+  'https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg',
+  'https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg',
+  'https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg'
+]
+
+let a = <Carousel src={d}/>
+// document.body.appendChild(a);
 a.mountTo(document.body)
